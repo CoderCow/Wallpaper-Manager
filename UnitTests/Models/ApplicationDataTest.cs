3183269bb954c65584ee534a840555a6cc7ec909
Kay Posmyk <KayPosmyk@gmx.de>
2015-11-16 01:02:40 +0100

    Added ApplicationData test.

---
+++ UnitTests/Models/ApplicationDataTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using WallpaperManager.Models;
+using Xunit;
+
+namespace UnitTests.Models {
+  public class ApplicationDataTest {
+    [Fact]
+    public void CtorShouldThrowOnNullArgument() {
+      Action constructA = () => new ApplicationData(null, new ObservableCollection<IWallpaperCategory>());
+      Action constructB = () => new ApplicationData(new Configuration(), null);
+
+      constructA.ShouldThrow<Exception>();
+      constructB.ShouldThrow<Exception>();
+    }
+
+    [Fact]
+    public void CtorShouldSetParametersProperly() {
+      IConfiguration configuration = new Configuration();
+      ObservableCollection<IWallpaperCategory> categories = new ObservableCollection<IWallpaperCategory>();
+      ApplicationData sut = new ApplicationData(configuration, categories);
+
+      sut.Configuration.Should().Be(configuration);
+      sut.WallpaperCategories.Should().BeSameAs(categories);
+    }
+  }
+}
